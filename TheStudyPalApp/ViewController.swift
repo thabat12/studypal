4fc6a1d92ebf7eba64714c334f75b3b4fec01d07
Abhinav Bichal <65791148+thabat12@users.noreply.github.com>
2025-03-10 16:33:23 -0500

    Initial Commit

---
+++ TheStudyPalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheStudyPalApp
+//
+//  Created by Abhi Bichal on 3/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
